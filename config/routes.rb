81fa8eca286fea6aa07934cdd4c0aca94419bd3f
kawashita kenji <leonardkenji@gmail.com>
2026-02-17 12:04:43 +0900

    show, new, edit done

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "/tasks", to: "tasks#index", as: :tasks
+  get "/tasks/new", to: "tasks#new", as: :new_task
+  post "/tasks", to: "tasks#create"
+  get "/tasks/:id", to: "tasks#show", as: :task
+  get "/tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "/tasks/:id", to: "tasks#update"
+
 end
